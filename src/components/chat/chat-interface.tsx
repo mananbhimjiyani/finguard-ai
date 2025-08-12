1e54b6be66560ba495de738ef52b03adef4a8ea6
Manan Bhimjiyani <mananbhimjiyani@gmail.com>
2025-08-12 11:17:26 +0000

    Still same upon my first question How can I optimize my contributions?

--- src/components/chat/chat-interface.tsx
+++ src/components/chat/chat-interface.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState, useRef, useEffect } from 'react';
@@ -72,7 +73,7 @@ export function ChatInterface() {
     <div className="flex flex-col h-full">
       <Card className="flex-1 flex flex-col min-h-0">
         <CardContent className="flex-1 flex flex-col p-4">
-          <ScrollArea className="flex-grow pr-4" ref={scrollAreaRef}>
+          <ScrollArea className="flex-1 min-h-0 pr-4" ref={scrollAreaRef}>
             <div className="space-y-6">
               {messages.length === 0 && (
                 <div className="text-center p-8 space-y-4">
